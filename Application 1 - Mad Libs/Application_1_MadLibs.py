9e9ad9f3c23384dfad7c4395848ce5525738b5fc
SereneMoons <86084846+SereneMoons@users.noreply.github.com>
2021-06-19 14:01:46 -0400

    Add files via upload

---
+++ Application 1 - Mad Libs/Application_1_MadLibs.py
@@ -0,0 +1,44 @@
+#This is Application 1 of Personal Projects
+#This Program will involve a Mad Lib.
+#The story was grabbed from www.woojr.com/ while I coded the story
+#These Projects Will be made from Syntax Memory and Knowledge with the help of google and no guides
+
+#Start
+print("Welcome To a Mad Lib About Chirstmas!")
+print('''        t
+       .#.
+      .###.
+     .#%##%.
+    .%##%###.
+   .##%###%##.  
+  .#%###%##%##.
+        #
+        #
+
+''')
+
+#User Input for Keywords
+print("Make a story with your own Imagination!")
+word_one = input("Enter a number: ")
+word_two = input("Enter a noun: ")
+word_three = input("Enter a verb: ")
+word_four = input("Enter an adjective: ")
+word_five = input("Enter a noun: ")
+word_six = input("Enter a verb: ")
+word_seven = input("enter an adjective: ")
+print("Halfway There!")
+word_eight = input("Enter a color: ")
+word_nine = input("Enter an adjective: ")
+word_ten = input("Enter a plural noun: ")
+word_eleven = input("Enter a plural noun: ")
+word_twelve = input("Enter a color: ")
+word_thirteen = input("Enter a part of the body: ")
+
+#Final Print of the Mad Libs Story
+print("")
+print("Here is the Story You Created!")
+print("==================================")
+print("")
+print(f"Did you know that Santa has {word_one} reindeer? Some of their names are {word_two}, {word_three}, {word_four} {word_five}, and {word_six}. Reindeer are {word_seven} mammals with {word_eight} fur that feels {word_nine} to the touch. They also have {word_ten} on top of their heads, and instead of feet and toes, they have {word_eleven}, like horses! Rudolph is a special reindeer; he has a(n) {word_twelve} {word_thirteen} that lights the way for Santa on Christmas Eve.")
+
+#End of Program
